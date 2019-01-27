8624704756f93305bb1a84dd971808695292818a
Oishi Bhattacharyya <oishibhattacharyya@yahoo.com>
2019-01-27 23:07:27 +0530

    Initial commit

---
+++ 21-10-2018.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print('Hello World')
+
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sun Oct 21 11:03:46 2018)---
+runfile('C:/Users/oc/.spyder-py3/21-10-2018.py', wdir='C:/Users/oc/.spyder-py3')
+run 21-10-2018.py
+a=3
+type(a)
+a=7.
+type(a)
+s = 'Hello'
+type(s)
+s1 = "World"
+s + s1
+s2 = s + ' ' + s1
+s2
+a.is_integer()
+b = 2 + 3j
+b.imag
+b.real
+type(b)
+d = True
+type(d)
+bts = ['jk', 'tae', 'jm', 'nj', 'hs', 'yg', 'sj']
+type(bts)
+fruits = ['apple', ...
+'orenji']
+bts.count()
+bts.count('jk')
+bts.index('tae')
+colors[3]
+bts[3]
+bts[-1]
+bts[-3]
+bts[0:2]
+bts[0:2:4]
+bts[0:3:5]
+bts[0:5:2]
+bts.pop()
+bts.append('sj')
+lol = ['Green', True, 123, [1,2,3,4,5]]
+lol[1][3]
+lol[0][3]
+empty = []
+dictionary = {'jk': 97, 'tae': 95, 'jm': 95, 'nj': 94, 'hs': 94, 'yg': 93, 'sj': 92}
+dictionary[2]
+dictionary.keys
+dictionary.keys()
+dictionary.values()
+dictionary.pop()
+dictionary['van']= 2000
+dictionary
+dictionary['jkbaby'] = ['cookie', 2001]
+lol.count(True)
+lol.index(123)
+tuple = ('world', False, 123, [1,2,3]]
+tuple = ('world', False, 123, [1,2,3])
+s = {'a', 'b', 'c'}
+s1 = {1, 2, 3, 'a'}
+s.union(s1)
+s.intersection(s1)
+#Ctrl+i = information window
+sh = "Hello World"
+sh.replace('l', 'w')
+sh
+sn = sh.replace('l', 'w')
+sn
+sh.replace('l','w',2)
+s.upper()
+sh.upper()
+sh.capitalize()
+sh.lower()
+sh.count('l')
